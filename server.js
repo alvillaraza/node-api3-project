574c26845c55879c7d34e25e3dcba5c8c3ef5b79
alexis villaraza <alexis.borja@gmail.com>
2020-03-11 12:49:38 -0700

    created middlewares

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+server.use(validateUserId());
+server.use(validateUser());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
@@ -14,6 +18,48 @@ function logger(req, res, next) {
   
   console.log(`${method} to ${endpoint}`);
   next();
+};
+  
+
+
+function validateUserId(id) {
+  return function (req, res, next) {
+    if (!id) {
+      res.status(400).json({message: "invalid user id"});
+    } else {
+      req.user = id;
+    }
+    next();
   };
+};
+
+
+
+function validateUser() {
+  return function (req, res, next) {
+    const { name } = req.body;
+    if (!body) {
+      res.status(400).json({ message: "missing user data" });
+    } if (!name) {
+      res.status(400).json({ message: "missing required name field" });
+    } else {
+      // res.status(201).json(return here???)
+    }
+    next();
+  };
+};
+
+function validatePost() {
+  return function (req, res, next) {
+    const { text } = req.body;
+    if (!body) {
+      res.status(400).json({message: "missing post data"})
+    } if (!text) {
+      res.status(400).json({ message: "missing required text field"})
+    } else {
+      // res.status(201).json(???)
+    }
+  }
+}
 
 module.exports = server;
